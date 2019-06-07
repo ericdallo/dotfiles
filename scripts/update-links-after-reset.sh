788cd9645303af48fa1f291a7f2b86ac7af823fe
Eric Dallo <ericdallo06@hotmail.com>
2019-06-07 10:09:42 -0300

    Add vscode files

--- scripts/update-links-after-reset.sh
+++ scripts/update-links-after-reset.sh
@@ -9,5 +9,7 @@ ln -sf ~/dev/dotfiles/alias/.zsh_aliases ~/.zsh_aliases
 ln -sf ~/dev/dotfiles/alias/.zshrc ~/.zshrc
 ln -sf ~/dev/dotfiles/git/.gitconfig ~/.gitconfig
 ln -sf ~/dev/dotfiles/alias/.functions ~/.functions
+ln -sf ~/dev/dotfiles/vscode/keybindings.json ~/.config/Code/User/keybindings.json
+ln -sf ~/dev/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
 
 echo "Links updated"
