31fc1177d50cfb4c0909cbb9aed27b0fa57fc591
Eric Dallo <ericdallo06@hotmail.com>
2019-11-18 23:01:37 -0300

    Add fresh-install.sh

---
+++ nix/fresh-install.sh
@@ -0,0 +1,3 @@
+sudo ln -sf ~/dev/dotfiles/nix/config-imports /etc/nixos/config-imports
+sudo ln -sf ~/dev/dotfiles/nix/configuration.nix /etc/nixos/configuration.nix
+sudo ln -sf ~/dev/dotfiles/nix/home.nix ~/.config/nixpkgs/home.nix
