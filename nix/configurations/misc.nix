d28e48b9d04d16b3e20ed5337917c94c4433f038
Eric Dallo <ericdallo06@hotmail.com>
2019-12-29 22:44:57 -0300

    Update xmodmap to nix way

--- nix/configurations/misc.nix
+++ nix/configurations/misc.nix
@@ -1,9 +1,16 @@
 { pkgs, ... }:
 
-{
+let
+  xmodmapLayout = pkgs.writeText "xkb-layout" ''
+    keycode 105 = Pause
+    keycode 127 = Control_R
+  '';
+in {
   boot = {
     tmpOnTmpfs = true;
   };
+
+  services.xserver.displayManager.sessionCommands = "${pkgs.xorg.xmodmap}/bin/xmodmap ${xmodmapLayout}";
 
   system.autoUpgrade = {
     enable = true;
