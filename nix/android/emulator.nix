d56a357de5c950cfe20b2776c4da363ad5bf72cd
Eric Dallo <ericdallo06@hotmail.com>
2020-01-30 10:16:35 -0300

    Update android version

--- nix/android/emulator.nix
+++ nix/android/emulator.nix
@@ -7,7 +7,7 @@ with import <nixpkgs> {
 
 androidenv.emulateApp {
   name = "nix-android";
-  platformVersion = "29";
+  platformVersion = "28";
   enableGPU = true;
   abiVersion = "x86";
   avdHomeDir = "$HOME";
