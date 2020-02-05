a46bd7a0012c0c4f3112292003f96ec4aa853c7e
Eric Dallo <ericdallo06@hotmail.com>
2020-02-05 00:37:49 -0300

    Remove unnecessary dependencies

--- nix/scripts/hover.nix
+++ nix/scripts/hover.nix
@@ -1,21 +1,16 @@
 with import <nixpkgs> {}; {
-    devEnv = stdenv.mkDerivation {
+    hoverDerivation = stdenv.mkDerivation {
         name = "hover-deps";
-        buildInputs = [ stdenv
-                        go
+        buildInputs = [ go
                         pkgconfig
                         glfw
-                        glxinfo
                         xorg.libX11
-                        xorg.libX11.dev
-                        xorg.libX11.dev.out
                         xorg.libXcursor
                         xorg.libXrandr
                         xorg.libXinerama
                         xorg.libXxf86vm
                         xorg.libXi
-                        haskellPackages.x11-xinput
-                        libGLU
-                        libGL];
+                        ];
     };
+
 }
