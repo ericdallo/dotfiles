56097668542fe13420bb6e879c1880399bedf206
Eric Dallo <ericdallo06@hotmail.com>
2020-09-28 15:07:54 -0300

    #emacs add omnisharp/lsp config

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -101,7 +101,7 @@
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
-       ;;csharp            ; unity, .NET, and mono shenanigans
+       (csharp +lsp)            ; unity, .NET, and mono shenanigans
        (dart +lsp)
        data              ; config/data formats
        ;;erlang            ; an elegant language for a more civilized age
