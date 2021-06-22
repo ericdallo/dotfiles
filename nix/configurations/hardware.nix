40b9db30a5d7628785320dac4625c7d24c67a20f
Eric Dallo <ericdallo06@hotmail.com>
2021-06-21 21:46:18 -0300

    [nix] Use pipewire instead of pulseaudio

--- nix/configurations/hardware.nix
+++ nix/configurations/hardware.nix
@@ -35,17 +35,17 @@
       };
     };
 
-    pulseaudio = {
-      enable = true;
-      package = pkgs.pulseaudioFull;
-      support32Bit = true;
+    # pulseaudio = {
+    #   enable = true;
+    #   package = pkgs.pulseaudioFull;
+    #   support32Bit = true;
 
-      # Enable extra bluetooth codecs.
-      extraModules = [ pkgs.pulseaudio-modules-bt ];
-      extraConfig = "
-        load-module module-switch-on-connect
-      ";
-    };
+    #   # Enable extra bluetooth codecs.
+    #   extraModules = [ pkgs.pulseaudio-modules-bt ];
+    #   extraConfig = "
+    #     load-module module-switch-on-connect
+    #   ";
+    # };
 
     # Extra OpenGL options.
     opengl = {
@@ -69,6 +69,41 @@
     fstrim = {
       enable = true;
       interval = "weekly";
+    };
+
+    pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+      # Bluetooth settings
+      media-session.config.bluez-monitor.rules = [
+        {
+          # Matches all cards
+          matches = [ { "device.name" = "~bluez_card.*"; } ];
+          actions = {
+            "update-props" = {
+              "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+              # mSBC is not expected to work on all headset + adapter combinations.
+              "bluez5.msbc-support" = true;
+              # "bluez5.sbc-xq-support" = true;
+            };
+          };
+        }
+        {
+          matches = [
+            # Matches all sources
+            { "node.name" = "~bluez_input.*"; }
+            # Matches all outputs
+            { "node.name" = "~bluez_output.*"; }
+          ];
+          actions = {
+            "node.pause-on-idle" = false;
+          };
+        }
+      ];
     };
 
     blueman = {
