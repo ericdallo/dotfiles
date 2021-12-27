7361e3c3942b63db122cc0cdbfe82908e827c2f8
Eric Dallo <ericdallo06@hotmail.com>
2021-12-27 09:47:07 -0300

    [nix] Update nubank dell host

--- nix/hosts/dell-nubank/default.nix
+++ nix/hosts/dell-nubank/default.nix
@@ -19,12 +19,12 @@
 
   fileSystems."/" =
     {
-      device = "/dev/disk/by-uuid/a6c734cf-c985-4322-9e1c-e88beb863027";
+      device = "/dev/disk/by-uuid/66408c59-a2fd-4333-b4c6-f6076f2458e9";
       fsType = "ext4";
     };
 
   swapDevices = [{
-    device = "/dev/disk/by-uuid/b1d04602-9d7b-4b73-91f0-3fbb58b7e47e";
+    device = "/dev/disk/by-uuid/7f5352c0-19c6-4327-8308-3d4a40ac368c";
   }];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
