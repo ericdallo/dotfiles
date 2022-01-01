3e3dff0fa06bc445faac8766b4023627fda9a2c6
Eric Dallo <ericdallo06@hotmail.com>
2022-01-01 14:27:59 -0300

    [emacs] disable emoji module

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -30,7 +30,7 @@
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
-       (emoji +unicode)  ; 🙂
+       ;;(emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE tags
        ;; hydra
        ;;indent-guides     ; highlighted indent columns
