32ab5699154db581e550ffe3aac5b60898fdc901
Eric Dallo <ericdallo06@hotmail.com>
2022-02-13 20:38:29 -0300

    nix: use wireplumber instead of media-session

--- nix/configurations/hardware.nix
+++ nix/configurations/hardware.nix
@@ -86,31 +86,8 @@
       };
       pulse.enable = true;
       # Bluetooth settings
-      media-session.config.bluez-monitor.rules = [
-        {
-          # Matches all cards
-          matches = [ { "device.name" = "~bluez_card.*"; } ];
-          actions = {
-            "update-props" = {
-              "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
-              # mSBC is not expected to work on all headset + adapter combinations.
-              "bluez5.msbc-support" = true;
-              # "bluez5.sbc-xq-support" = true;
-            };
-          };
-        }
-        {
-          matches = [
-            # Matches all sources
-            { "node.name" = "~bluez_input.*"; }
-            # Matches all outputs
-            { "node.name" = "~bluez_output.*"; }
-          ];
-          actions = {
-            "node.pause-on-idle" = false;
-          };
-        }
-      ];
+      media-session.enable = false;
+      wireplumber.enable = true;
     };
 
     blueman = {
