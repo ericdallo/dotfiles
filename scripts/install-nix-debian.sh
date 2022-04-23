4c66be893428ef9d0c7abd7755680360493bf961
Eric Dallo <ericdallo06@hotmail.com>
2022-04-23 19:41:05 -0300

    nix: update debian scrip

--- scripts/install-nix-debian.sh
+++ scripts/install-nix-debian.sh
@@ -4,7 +4,12 @@ sudo apt install vim git build-essential dkms linux-headers-$(uname -r)
 git clone https://github.com/ericdallo/dotfiles.git ~/.dotfiles
 
 sudo mkdir /nix
-sudo chown $USERNAME /nix
+sudo chown "$USERNAME" /nix
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
+./home/"$USERNAME"/.nix-profiles/etc/profile.d/nix.sh
+
+echo "export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}" >> ~/.bashrc
 nix-env -iA nixpkgs.home-manager
-echo "export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}" > ~/.bashrc
+
+mkdir -p ~/.config/nix
+echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
