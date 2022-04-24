89beb60ab828bd5fa3bae17b3fdde44697fe8f4b
Eric Dallo <ericdallo06@hotmail.com>
2022-04-24 17:53:14 -0300

    nix: update nubank debian script

--- scripts/install-nix-debian.sh
+++ scripts/install-nix-debian.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-sudo apt install vim git build-essential dkms linux-headers-$(uname -r) bspwm
+set -ve
+
+echo "deb http://deb.debian.org/debian bullseye-backports main contrib non-free" >> /etc/apt/sources.list
+
+sudo apt update
+sudo apt install -y vim git build-essential dkms linux-headers-$(uname -r) bspwm bullseye-backports linux-image-amd64
 git clone https://github.com/ericdallo/dotfiles.git ~/.dotfiles
 
 sudo mkdir /nix
 sudo chown "$USERNAME" /nix
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
-./home/"$USERNAME"/.nix-profiles/etc/profile.d/nix.sh
+source "/home/$USERNAME/.nix-profiles/etc/profile.d/nix.sh"
 
 echo "export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}" >> ~/.bashrc
 nix-env -iA nixpkgs.home-manager
@@ -14,7 +19,7 @@ nix-env -iA nixpkgs.home-manager
 mkdir -p ~/.config/nix
 echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
 
-cd ~/.dotfiles
+cd ~/.dotfiles/nix
 home-manager switch --flake .
 
 echo "export XDG_DATA_DIRS=$HOME/.nix-profile/share:$HOME/.share:\"${XDG_DATA_DIRS:-/usr/local/share/:/usr/share/}\"" >> ~/.profile
@@ -22,5 +27,9 @@ echo "export XDG_DATA_DIRS=$HOME/.nix-profile/share:$HOME/.share:\"${XDG_DATA_DI
 
 echo "if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then . $HOME/.nix-profile/etc/profile.d/nix.sh; fi" >> ~/.zprofile
 
-sudo echo "/home/greg/.nix-profile/bin/zsh" >> /etc/shells
+echo "/home/greg/.nix-profile/bin/zsh" | sudo tee -a /etc/shells > /dev/null
 sudo chsh -s /home/greg/.nix-profile/bin/zsh
+
+# Natural scrolling
+echo "xinput set-prop 13 324 1" >> ~/.profile
+echo "xinput set-prop 13 324 1" >> ~/.zprofile
