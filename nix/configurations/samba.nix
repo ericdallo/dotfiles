8e63c6086afaf3e23c11a0272f0ccd8105b48c5b
Eric Dallo <ericdallo06@hotmail.com>
2025-01-12 12:47:06 -0300

    nix: samba tmp

--- nix/configurations/samba.nix
+++ nix/configurations/samba.nix
@@ -4,21 +4,21 @@
   services.samba = {
     enable = true;
     openFirewall = true;
-    extraConfig = ''
-      workgroup = SAMBA
-      server string = %h server (Samba %v)
-      netbios name = smbnix
-      security = user
-      #use sendfile = yes
-      #max protocol = smb2
-      # note: localhost is the ipv6 localhost ::1
-      hosts allow = 192.168.15. 127.0.0.1 localhost
+    # settings = ''
+    #   workgroup = SAMBA
+    #   server string = %h server (Samba %v)
+    #   netbios name = smbnix
+    #   security = user
+    #   #use sendfile = yes
+    #   #max protocol = smb2
+    #   # note: localhost is the ipv6 localhost ::1
+    #   hosts allow = 192.168.15. 127.0.0.1 localhost
 
-      hosts deny = 0.0.0.0/0
-      guest account = nobody
-      map to guest = bad user
-    '';
-    shares = {
+    #   hosts deny = 0.0.0.0/0
+    #   guest account = nobody
+    #   map to guest = bad user
+    # '';
+    settings = {
       public = {
         path = "/mnt/samba/Public";
         browseable = "yes";
