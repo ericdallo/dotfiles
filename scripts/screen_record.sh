c151a0942236a32b6381b0563eab13d46093f94d
Eric Dallo <ericdallo06@hotmail.com>
2025-08-28 18:11:10 -0300

    scripts: improve screen recorder

--- scripts/screen_record.sh
+++ scripts/screen_record.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+
 filename=~/Videos/recording-$(date -u +"%Y-%m-%d.%H.%M.%S")
 
 wf-recorder -f "$filename".mp4 -y -g "$(slurp)"
 gifski -o "$filename".gif "$filename".mp4
-rm "$filename".mp4
 notify-send "Saved as $filename.gif"
